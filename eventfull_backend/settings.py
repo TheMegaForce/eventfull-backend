0d52dc4e86d240bac701b28cd8daf1559bac3679
Ishan Azad <ishansazad@gmail.com>
2023-11-16 14:18:36 -0800

    ALLOWED_HOSTS fix

--- eventfull_backend/settings.py
+++ eventfull_backend/settings.py
@@ -37,6 +37,9 @@ DEBUG = True if os.environ['MODE'] == 'dev' else False
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
